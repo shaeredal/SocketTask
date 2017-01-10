7c772fac455106d9a75a3f7990460fd49d3b4bf6
shaeredal <shaeredal@gmail.com>
2017-01-10 14:34:54 +0300

    Initial.

---
+++ SocketTask/SocketTask/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace SocketTask.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
